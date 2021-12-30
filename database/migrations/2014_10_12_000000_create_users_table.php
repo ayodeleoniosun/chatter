ced5ff7a347fe033963b8b9ebb3cac8ed0dc400b
Ayodele Oniosun <ayodele@ogunlabs.com>
2021-12-30 15:02:03 +0100

    Add migration files

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email_address')->unique();
+            $table->string('phone_number');
+            $table->string('password');
+            $table->string('bearer_token');
+            $table->dateTime('token_expires_at');
+            $table->rememberToken();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2021_12_30_134529_create_private_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrivateMessagesTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('private_messages', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sender_id')->constrained('users');
+            $table->foreignId('recipient_id')->constrained('users');
+            $table->longText('content');
+            $table->string('content_type')->default('text');
+            $table->foreignId('file_id')->constrained('files');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('private_messages');
+    }
+}
