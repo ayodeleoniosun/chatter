f8a69faed139b315cbae6a2778e2e98763e41b7e
Ayodele Oniosun <ayodele@ogunlabs.com>
2022-01-10 01:28:00 +0100

    Install laravel sanctum

--- database/migrations/2021_12_30_134529_create_chats_table.php
+++ database/migrations/2021_12_30_134529_create_chats_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePrivateMessagesTable extends Migration
+class CreateChatsTable extends Migration
 {
 
     /**
@@ -14,7 +14,7 @@ class CreatePrivateMessagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('private_messages', function (Blueprint $table) {
+        Schema::create('chats', function (Blueprint $table) {
             $table->id();
             $table->foreignId('sender_id')->constrained('users');
             $table->foreignId('recipient_id')->constrained('users');
