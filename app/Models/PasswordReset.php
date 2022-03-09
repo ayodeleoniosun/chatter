41b166829e4e9fe541e3275a0f922314f5cbb18a
Ayodele Oniosun <heywire4u@gmail.com>
2022-03-09 03:07:14 +0100

    feat: import softdeletes for model

--- app/Models/PasswordReset.php
+++ app/Models/PasswordReset.php
@@ -4,12 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PasswordReset extends Model
 {
-    use SoftDeletes;
-
     use HasFactory;
 
     protected $table = 'password_resets';
--- app/Models/UserProfilePicture.php
+++ app/Models/UserProfilePicture.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class UserProfilePicture extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'user_profile_pictures';
 
     protected $guarded = ['id'];
