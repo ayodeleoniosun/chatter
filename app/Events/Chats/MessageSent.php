59d84d3984c0a69d463b50b2f317080bdffcc3db
Ayodele Oniosun <heywire4u@gmail.com>
2022-03-27 21:19:28 +0100

    Fix empty payload sent to pusher

--- app/Events/Chats/MessageSent.php
+++ app/Events/Chats/MessageSent.php
@@ -2,7 +2,9 @@
 
 namespace App\Events\Chats;
 
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
@@ -13,7 +15,7 @@ class MessageSent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    protected array $data;
+    public array $data;
 
     /**
      * Create a new event instance.
